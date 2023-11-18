8436660d2c65731873bff1c73b7086cc2e65885f
mahmmud110 <mahmud110@gmail.com>
2023-11-18 03:29:51 -0500

    first save

---
+++ src/main/java/com/generic/Login.java
@@ -0,0 +1,17 @@
+package com.generic;
+
+public class Login {
+	static int salary = 5000;
+	static String name ="Alam";
+	static double price =3.75;
+	
+	Login obj = new Login();
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println(salary);
+		System.out.println(name);
+		System.out.println(price);
+	}
+
+}
